a6ebd9c6125c82ed1fdd87adaea0206f50f02434
TR-Fur <78869478+TR-Fur@users.noreply.github.com>
2022-03-04 10:50:37 -0600

    Started tile movement

---
+++ Player.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+
+var tile_size = 64
+var inputs = {"ui_right": Vector2.RIGHT,
+"ui_left": Vector2.LEFT,
+"ui_up": Vector2.UP,
+"ui_down": Vector2.DOWN}
+
+func _ready():
+	pass # Replace with function body.
