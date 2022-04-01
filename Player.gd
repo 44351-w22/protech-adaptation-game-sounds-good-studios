2c7dda78d32bd2f707c3648208164057e91f5f63
Jacob Daleske <60079607+jdaleske13@users.noreply.github.com>
2022-03-31 23:25:25 -0500

    Made it to where the world will detect what tile the player is on

--- Player.gd
+++ Player.gd
@@ -11,16 +11,23 @@ var inputs = {"ui_right": Vector2.RIGHT,
 "ui_left": Vector2.LEFT,
 "ui_up": Vector2.UP,
 "ui_down": Vector2.DOWN}
+signal Collision
+var start_Position_x = 608
+var start_Position_y = 96
 
 func _ready():
-	position.x = 608
-	position.y = 96
+	position.x = start_Position_x
+	position.y = start_Position_y
 
 func move(dir):
 	rayDown.cast_to = inputs[dir] * tile_size
 	rayDown.force_raycast_update()
 	if !rayDown.is_colliding():
 		move_tween(dir)
+		
+	#if $CollisionShape2D.position.x != start_Position_x or $CollisionShape2D.position.y != start_Position_y:
+		#emit_signal("Collision")
+		
 		
 func move_tween(dir):
 	tween.interpolate_property(self, "position", position, position + inputs[dir] * tile_size, 1.0/speed, tween.TRANS_SINE, tween.EASE_IN_OUT)
@@ -32,3 +39,10 @@ func _unhandled_input(event):
 	for dir in inputs.keys():
 		if event.is_action_pressed(dir):
 			move(dir)
+			
+
+func _process(delta):
+	if $CollisionShape2D.position.x != start_Position_x or $CollisionShape2D.position.y != start_Position_y:
+		emit_signal("Collision")
+
+
--- World.gd
+++ World.gd
@@ -10,3 +10,15 @@ func _ready():
 func move_camera(p):
 	cam.get_parent().remove_child(cam)
 	p.add_child(cam)
+
+
+
+func _on_Player1_Collision():
+	var playerX = $Player1/CollisionShape2D.global_position.x
+	var playerY = $Player1/CollisionShape2D.global_position.y
+	var tile_coord = $TileMap.world_to_map($Player1.global_position)
+	var tile_index = $TileMap.get_cellv(tile_coord)
+	var tile = $TileMap.tile_set.tile_get_name(tile_index)
+	print(tile)
+	#var tile_name = $TileMap.tile_set.tile_get_name(tile_id)
+	#print(tile_name)
