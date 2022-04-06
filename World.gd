abffb5f403ba945494c131b8e3f85a39623bc01b
Daniel J Crowley <dcrow2814@gmail.com>
2022-04-06 13:24:00 -0500

    total of 4 players

--- World.gd
+++ World.gd
@@ -3,8 +3,12 @@ extends Node
 onready var cam = $GameCam
 onready var p1 = $Player1
 onready var p2 = $Player2
+onready var p3 = $Player3
+onready var p4 = $Player4
 onready var p1Sprite = $Player1/Sprite
 onready var p2Sprite = $Player2/Sprite
+onready var p3Sprite = $Player3/Sprite
+onready var p4Sprite = $Player4/Sprite
 onready var dice = $CanvasLayer/Dice_Roller
 onready var totalDiceRoll = dice.totalRoll
 onready var currentPlayer = p1
@@ -20,6 +24,8 @@ var skins = [preload("res://assets/players/pieceGreen_single00.png"),
 func _ready():
 	p1Sprite.texture = skins[0]
 	p2Sprite.texture = skins[1]
+	p3Sprite.texture = skins[2]
+	p4Sprite.texture = skins[4]
 	move_camera(p1)
 
 func move_camera(p):
@@ -52,10 +58,15 @@ func _on_Button_pressed():
 	currentPlayer.movesLeft = totalDiceRoll
 
 func _on_EndTurn_pressed():
+	currentPlayer.movesLeft = 0
 	currentPlayer = nextPlayer
 	if currentPlayer == p1:
 		nextPlayer = p2
 	elif currentPlayer == p2:
+		nextPlayer = p3
+	elif currentPlayer == p3:
+		nextPlayer = p4
+	elif currentPlayer == p4:
 		nextPlayer = p1
 	else:
 		print("error")
