90b7ac3585c05b5f49a77be192aa11203a0163e8
Daniel J Crowley <61795248+TheMusicMan28@users.noreply.github.com>
2022-04-20 19:31:49 -0500

    Added a Start Screen

--- World.gd
+++ World.gd
@@ -9,15 +9,15 @@ onready var p1Sprite = $Player1/Sprite
 onready var p2Sprite = $Player2/Sprite
 onready var p3Sprite = $Player3/Sprite
 onready var p4Sprite = $Player4/Sprite
-onready var dice = $CanvasLayer/Dice_Roller
-onready var diceContainer = $CanvasLayer/Dice_Roller/Container
+onready var dice = $UI/Dice_Roller
+onready var diceContainer = $UI/Dice_Roller/Container
 onready var totalDiceRoll = dice.totalRoll
 onready var currentPlayer = Global.current_player
 onready var nextPlayer = Global.next_player
 onready var music = $SoundEffects/Music
 onready var laugh = $SoundEffects/Laugh
 onready var cheer = $SoundEffects/Cheer
-onready var playerLabel = $CanvasLayer/CurrentPlayer
+onready var playerLabel = $UI/CurrentPlayer
 var rng = RandomNumberGenerator.new()
 var map = 0
 var hidden = false
@@ -36,56 +36,56 @@ func _ready():
 	p2Sprite.texture = skins[1]
 	p3Sprite.texture = skins[2]
 	p4Sprite.texture = skins[4]
-	start_game()
+	currentPlayer = p1
 
 func _physics_process(delta):
 	#Adjust Moves Left
 	if currentPlayer.movesLeft == 0:
-		$CanvasLayer/MovesLeft.text = "Moves Left: 0"
+		$UI/MovesLeft.text = "Moves Left: 0"
 	elif currentPlayer.movesLeft == 1:
-		$CanvasLayer/MovesLeft.text = "Moves Left: 1"
+		$UI/MovesLeft.text = "Moves Left: 1"
 	elif currentPlayer.movesLeft == 2:
-		$CanvasLayer/MovesLeft.text = "Moves Left: 2"
+		$UI/MovesLeft.text = "Moves Left: 2"
 	elif currentPlayer.movesLeft == 3:
-		$CanvasLayer/MovesLeft.text = "Moves Left: 3"
+		$UI/MovesLeft.text = "Moves Left: 3"
 	elif currentPlayer.movesLeft == 4:
-		$CanvasLayer/MovesLeft.text = "Moves Left: 4"
+		$UI/MovesLeft.text = "Moves Left: 4"
 	elif currentPlayer.movesLeft == 5:
-		$CanvasLayer/MovesLeft.text = "Moves Left: 5"
+		$UI/MovesLeft.text = "Moves Left: 5"
 	elif currentPlayer.movesLeft == 6:
-		$CanvasLayer/MovesLeft.text = "Moves Left: 6"
+		$UI/MovesLeft.text = "Moves Left: 6"
 	elif currentPlayer.movesLeft == 7:
-		$CanvasLayer/MovesLeft.text = "Moves Left: 7"
+		$UI/MovesLeft.text = "Moves Left: 7"
 	elif currentPlayer.movesLeft == 8:
-		$CanvasLayer/MovesLeft.text = "Moves Left: 8"
+		$UI/MovesLeft.text = "Moves Left: 8"
 	elif currentPlayer.movesLeft == 9:
-		$CanvasLayer/MovesLeft.text = "Moves Left: 9"
+		$UI/MovesLeft.text = "Moves Left: 9"
 	elif currentPlayer.movesLeft == 10:
-		$CanvasLayer/MovesLeft.text = "Moves Left: 10"
+		$UI/MovesLeft.text = "Moves Left: 10"
 	elif currentPlayer.movesLeft == 11:
-		$CanvasLayer/MovesLeft.text = "Moves Left: 11"
+		$UI/MovesLeft.text = "Moves Left: 11"
 	elif currentPlayer.movesLeft == 12:
-		$CanvasLayer/MovesLeft.text = "Moves Left: 12"
+		$UI/MovesLeft.text = "Moves Left: 12"
 	else:
 		currentPlayer.movesLeft = 0
-		$CanvasLayer/MovesLeft.text = "Moves Left: 0"
+		$UI/MovesLeft.text = "Moves Left: 0"
 	
 	#Adjust Maps Found
 	if currentPlayer.totalMapsFound == 0:
-		$CanvasLayer/MapsFound.text = "Maps Found: 0"
+		$UI/MapsFound.text = "Maps Found: 0"
 	elif currentPlayer.totalMapsFound == 1:
-		$CanvasLayer/MapsFound.text = "Maps Found: 1"
+		$UI/MapsFound.text = "Maps Found: 1"
 	elif currentPlayer.totalMapsFound == 2:
-		$CanvasLayer/MapsFound.text = "Maps Found: 2"
+		$UI/MapsFound.text = "Maps Found: 2"
 	elif currentPlayer.totalMapsFound == 3:
-		$CanvasLayer/MapsFound.text = "Maps Found: 3"
+		$UI/MapsFound.text = "Maps Found: 3"
 	elif currentPlayer.totalMapsFound == 4:
-		$CanvasLayer/MapsFound.text = "Maps Found: 4"
+		$UI/MapsFound.text = "Maps Found: 4"
 	elif currentPlayer.totalMapsFound == 5:
-		$CanvasLayer/MapsFound.text = "Maps Found: 5"
+		$UI/MapsFound.text = "Maps Found: 5"
 	else:
 		currentPlayer.totalMapsFound == 0
-		$CanvasLayer/MapsFound.text = "Maps Found: 0"
+		$UI/MapsFound.text = "Maps Found: 0"
 	
 	#Update Player Label
 	if currentPlayer == p1:
@@ -126,10 +126,10 @@ func detect():
 		laugh.play()
 		currentPlayer.position.x = 608
 		currentPlayer.position.y = 96
-		$CanvasLayer/Message.text = "Position Reset!!!"
-		$CanvasLayer/Message.show()
+		$UI/Message.text = "Position Reset!!!"
+		$UI/Message.show()
 		yield(get_tree().create_timer(3.0), "timeout")
-		$CanvasLayer/Message.hide()
+		$UI/Message.hide()
 	elif tile == "Pale Blue Brick Floor.png 1":
 		cheer.play()
 		select_random_map()
@@ -137,35 +137,35 @@ func detect():
 		laugh.play()
 		if currentPlayer.key_found == true:
 			currentPlayer.key_found == false
-			$CanvasLayer/Message.text = "Key Lost"
-			$CanvasLayer/Message.show()
+			$UI/Message.text = "Key Lost"
+			$UI/Message.show()
 			yield(get_tree().create_timer(3.0), "timeout")
-			$CanvasLayer/Message.hide()
+			$UI/Message.hide()
 		else:
 			currentPlayer.position.x = 608
 			currentPlayer.position.y = 96
-			$CanvasLayer/Message.text = "Position Reset"
-			$CanvasLayer/Message.show()
+			$UI/Message.text = "Position Reset"
+			$UI/Message.show()
 			yield(get_tree().create_timer(3.0), "timeout")
-			$CanvasLayer/Message.hide()
+			$UI/Message.hide()
 	elif tile == "Red Brick Floor.png 3":
 		print("red")
 	elif tile == "Yellow Brick Floor.png 4":
 		cheer.play()
 		if(currentPlayer.map1_found == true):
 			Global.map1_found = true
-		elif(currentPlayer.map2_found == true):
+		if(currentPlayer.map2_found == true):
 			Global.map2_found = true
-		elif(currentPlayer.map3_found == true):
+		if(currentPlayer.map3_found == true):
 			Global.map3_found = true
-		elif(currentPlayer.map4_found == true):
+		if(currentPlayer.map4_found == true):
 			Global.map4_found = true
-		elif(currentPlayer.map5_found == true):
+		if(currentPlayer.map5_found == true):
 			Global.map5_found = true
-		$CanvasLayer/Message.text = "Minigame Beginning. \nFIND THE KEY"
-		$CanvasLayer/Message.show()
+		$UI/Message.text = "Minigame Beginning. \nFIND THE KEY"
+		$UI/Message.show()
 		yield(get_tree().create_timer(3.0), "timeout")
-		$CanvasLayer/Message.hide()
+		$UI/Message.hide()
 		Global.goto_scene("res://FindTheKey_Minigame1/FindTheKey.tscn")
 		hide_all()
 		#call_deferred("show_all")
@@ -173,10 +173,10 @@ func detect():
 
 func select_random_map():
 	if currentPlayer.map1_found == true and currentPlayer.map2_found == true and currentPlayer.map3_found == true and currentPlayer.map4_found == true and currentPlayer.map5_found == true:
-		$CanvasLayer/Message.text = "All Maps Found"
-		$CanvasLayer/Message.show()
+		$UI/Message.text = "All Maps Found"
+		$UI/Message.show()
 		yield(get_tree().create_timer(3.0), "timeout")
-		$CanvasLayer/Message.hide()
+		$UI/Message.hide()
 	else:
 		map = rng.randi_range(1, 5)
 		if map == 1:
@@ -184,55 +184,60 @@ func select_random_map():
 				select_random_map()
 			else:
 				currentPlayer.map1_found = true
-				$CanvasLayer/Message.text = "Map of Maze 1 Found"
-				$CanvasLayer/Message.show()
+				currentPlayer.totalMapsFound += 1
+				$UI/Message.text = "Map of Maze 1 Found"
+				$UI/Message.show()
 				yield(get_tree().create_timer(3.0), "timeout")
-				$CanvasLayer/Message.hide()
+				$UI/Message.hide()
 		elif map == 2:
 			if currentPlayer.map2_found == true:
 				select_random_map()
 			else:
 				currentPlayer.map2_found = true
-				$CanvasLayer/Message.text = "Map of Maze 2 Found"
-				$CanvasLayer/Message.show()
+				currentPlayer.totalMapsFound += 1
+				$UI/Message.text = "Map of Maze 2 Found"
+				$UI/Message.show()
 				yield(get_tree().create_timer(3.0), "timeout")
-				$CanvasLayer/Message.hide()
+				$UI/Message.hide()
 		elif map == 3:
 			if currentPlayer.map3_found == true:
 				select_random_map()
 			else:
 				currentPlayer.map3_found = true
-				$CanvasLayer/Message.text = "Map of Maze 3 Found"
-				$CanvasLayer/Message.show()
+				currentPlayer.totalMapsFound += 1
+				$UI/Message.text = "Map of Maze 3 Found"
+				$UI/Message.show()
 				yield(get_tree().create_timer(3.0), "timeout")
-				$CanvasLayer/Message.hide()
+				$UI/Message.hide()
 		elif map == 4:
 			if currentPlayer.map4_found == true:
 				select_random_map()
 			else:
 				currentPlayer.map4_found = true
-				$CanvasLayer/Message.text = "Map of Maze 4 Found"
-				$CanvasLayer/Message.show()
+				currentPlayer.totalMapsFound += 1
+				$UI/Message.text = "Map of Maze 4 Found"
+				$UI/Message.show()
 				yield(get_tree().create_timer(3.0), "timeout")
-				$CanvasLayer/Message.hide()
+				$UI/Message.hide()
 		elif map == 5:
 			if currentPlayer.map5_found == true:
 				select_random_map()
 			else:
 				currentPlayer.map5_found = true
-				$CanvasLayer/Message.text = "Map of Maze 5 Found"
-				$CanvasLayer/Message.show()
+				currentPlayer.totalMapsFound += 1
+				$UI/Message.text = "Map of Maze 5 Found"
+				$UI/Message.show()
 				yield(get_tree().create_timer(3.0), "timeout")
-				$CanvasLayer/Message.hide()
+				$UI/Message.hide()
 		else:
 			pass
 
 func player_turn():
 	dice.visible = true
 	if currentPlayer.key_found == true:
-		$CanvasLayer/KeyFound.visible = true
+		$UI/KeyFound.visible = true
 	else:
-		$CanvasLayer/KeyFound.visible = false
+		$UI/KeyFound.visible = false
 
 func _on_Button_pressed():
 	yield(get_tree().create_timer(2.0), "timeout")
@@ -259,10 +264,10 @@ func end_turn():
 		move_camera(currentPlayer)
 		player_turn()
 	else:
-		$CanvasLayer/Message.text = "You still have moves left. Cannot pass turn until all moves are used up"
-		$CanvasLayer/Message.show()
+		$UI/Message.text = "You still have moves left. Cannot pass turn until all moves are used up"
+		$UI/Message.show()
 		yield(get_tree().create_timer(3.0), "timeout")
-		$CanvasLayer/Message.hide()
+		$UI/Message.hide()
 
 func hide_all():
 	hidden = true
@@ -273,11 +278,11 @@ func hide_all():
 	$Player2.hide()
 	$Player3.hide()
 	$Player4.hide()
-	$CanvasLayer/Dice_Roller.hide()
-	$CanvasLayer/EndTurn.hide()
-	$CanvasLayer/MapsFound.hide()
-	$CanvasLayer/KeyFound.hide()
-	$CanvasLayer/MovesLeft.hide()
+	$UI/Dice_Roller.hide()
+	$UI/EndTurn.hide()
+	$UI/MapsFound.hide()
+	$UI/KeyFound.hide()
+	$UI/MovesLeft.hide()
 	playerLabel.hide()
 	$OutSideTileMap.hide()
 	music.stop()
@@ -291,10 +296,10 @@ func show_all():
 	$Player2.show()
 	$Player3.show()
 	$Player4.show()
-	$CanvasLayer/Dice_Roller.show()
-	$CanvasLayer/EndTurn.show()
-	$CanvasLayer/MapsFound.show()
-	$CanvasLayer/MovesLeft.show()
+	$UI/Dice_Roller.show()
+	$UI/EndTurn.show()
+	$UI/MapsFound.show()
+	$UI/MovesLeft.show()
 	playerLabel.show()
 	$OutSideTileMap.show()
 	music.play()
@@ -302,6 +307,7 @@ func show_all():
 	Global.maze_beaten = false
 
 func start_game():
+<<<<<<< Updated upstream
 	p1.position.x = p1.start_Position_x
 	p1.position.y = p1.start_Position_y
 	p2.position.x = p2.start_Position_x
@@ -310,8 +316,26 @@ func start_game():
 	p3.position.y = p3.start_Position_y
 	p4.position.x = p4.start_Position_x
 	p4.position.y = p4.start_Position_y
+=======
+	music.play()
+	$UI/EndTurn.show()
+	$UI/MapsFound.show()
+	$UI/MovesLeft.show()
+	$UI/CurrentPlayer.show()
+>>>>>>> Stashed changes
 	currentPlayer = p1
 	nextPlayer = p2
 	move_camera(p1)
 	music.play()
 	player_turn()
+
+func _on_Begin_pressed():
+	$StartScreen/ColorRect.hide()
+	$StartScreen/MainMenu.hide()
+	start_game()
+
+func _on_Options_pressed():
+	pass
+
+func _on_Exit_pressed():
+	get_tree().quit()
